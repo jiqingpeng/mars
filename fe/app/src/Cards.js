e443d7b2c6fd51970b52f6fb0a45f765b8eede96
吉庆鹏 <jiqingpeng@dding.cn>
2018-12-06 17:53:10 +0800

    change list

--- fe/app/src/Cards.js
+++ fe/app/src/Cards.js
@@ -1,40 +1,97 @@
 import React, { Component } from 'react';
 import { Card, WhiteSpace,Button  } from 'antd-mobile';
-
-function genData(pIndex = 0) {
+import axios from 'axios';
+function genData(){
+  
   const dataBlob = [];
-  for (var i = 0; i < 10; i++) {
-    dataBlob.push(
-      <Card full key={i}>
-          <Card.Header
-            title="This is title"
-            thumb="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"
-            extra={<Button size="small">+ 关注</Button>}
-          />
-          <Card.Body>
-            <div className="card-img"><img src="https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png"/></div>
-          </Card.Body>
-          <Card.Footer content="footer content" extra={<div>extra footer content</div>} />
-      </Card>
-    );
+  // for (var i = 0; i < 10; i++) {
+  //   dataBlob.push(
+  //       <Card full key={i}>
+  //           <Card.Header
+  //             title="This is title"
+  //             thumb="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"
+  //             extra={<Button size="small">+ 关注</Button>}
+  //           />
+  //           <Card.Body>
+  //             <div className="card-img"><img src="https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png"/></div>
+  //           </Card.Body>
+  //           <Card.Footer content="footer content" extra={<div>extra footer content</div>} />
+  //       </Card>
+  //   );
+  // } 
+  // return dataBlob; 
+  
+ axios.get('http://47.100.30.67:7001/test').then(function(){
+    for (var i = 0; i < 10; i++) {
+      dataBlob.push(
+          <Card full key={i}>
+              <Card.Header
+                title="This is title"
+                thumb="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"
+                extra={<Button size="small">+ 关注</Button>}
+              />
+              <Card.Body>
+                <div className="card-img"><img src="https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png"/></div>
+              </Card.Body>
+              <Card.Footer content="footer content" extra={<div>extra footer content</div>} />
+          </Card>
+      );
+      
+    }
+    return dataBlob
+    
+  })
+  
+
+  return dataBlob
+ 
   }
-  return dataBlob;
-}
+ 
 class Cards extends React.Component {
   constructor(props) {
     super(props);
-   
-
     this.state = {
-     
+      genData:[]
     };
+    
   }
+  genData(){
+    let that =this;
+    axios.get('http://47.100.30.67:7001/test')
+    .then(function(res){
+      let dataBlob = [];
+      for (var i = 0; i < res.data.length; i++) {
+        dataBlob.push(
+            <Card full key={i}>
+                <Card.Header
+                  title="This is title"
+                  thumb="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"
+                  extra={<Button size="small">+ 关注</Button>}
+                />
+                <Card.Body>
+                  <div className="card-img"><img src={res.data[i].url}/></div>
+                </Card.Body>
+                <Card.Footer content="footer content" extra={<div>extra footer content</div>} />
+            </Card>
+        );
+        
+      }
+      that.setState({
+        genData:dataBlob
+      })
+
+    })
+  }
+
   render() {
     return (
       <div>
-        {genData()}
-      </div>
+        {this.state.genData}
+       </div>
   );
+  }
+  componentWillMount(){
+    this.genData();
   }
 }
 
